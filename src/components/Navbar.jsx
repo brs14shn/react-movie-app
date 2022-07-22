d44c58b87caf1fb4d87fbe616f20d50511835cdf
brs14shn <brsshn292929@gmail.com>
2022-07-22 12:45:09 +0300

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const currentUser = { displayName: "barıs sahin" };
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg ">
+        <div className="container-fluid">
+          <Link className="navbar-brand text-white" to={"/"}>
+            <h4>React MovieApp</h4>
+          </Link>
+        </div>
+        <div className="d-flex text-white align-items-center">
+          {currentUser ? (
+            <>
+              <h5 className="mb-0 text-capitalize">
+                {currentUser.displayName}
+              </h5>
+              <button className="ms-2 btn btn-outline-light">Logout</button>
+            </>
+          ) : null}
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
