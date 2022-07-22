b8549c4f09c1a333f85120c16bc27522c08a9b5c
brs14shn <brsshn292929@gmail.com>
2022-07-22 13:03:41 +0300

    Register atarted

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="d-flex justify-content-center ">
+      <div className="form-image d-none d-md-block">
+        <img src={"https://picsum.photos/800/800"} alt="" />
+      </div>
+      <div className="register-form">
+        <h1 className="form-title display-3">Register</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
